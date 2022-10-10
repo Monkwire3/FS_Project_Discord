951978fd0d438b7c9ffb3e0efbb63ae17a851eca
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-10 14:35:22 -0400

    fixed sessions controller

---
+++ discourse/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+    def show
+        if current_user
+            @user = current_user
+            render 'api/users/show'
+        else
+            render json: { user: nil }
+        end
+    end
+
+    def create
+        debugger
+        @user = User.find_by_credentials(params[:credential], params[:password])
+
+        if @user
+            login(@user)
+            render 'api/users/show'
+        else
+            render json: { errors: ['Invalid credentials']}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        logout
+        render json: { message: 'user successfully logges out'}
+    end
+end
--- discourse/app/controllers/application_controller.rb
+++ discourse/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
     def current_user
-        @current_user ||= User.find_by_credentials(session_token: session[:session_token])
+        @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
     def login(user)
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::API
     end
 
     def test
-        if params.has_key(:login)
+        if params.has_key?(:login)
             login(User.first)
         elsif params.has_key?(:logout)
             logout
@@ -23,6 +23,7 @@ class ApplicationController < ActionController::API
         if current_user
             render json: { user: current_user.slice('id', 'username', 'session_token') }
         else
+            puts 'else'
             render json: ['No current user']
         end
     end
