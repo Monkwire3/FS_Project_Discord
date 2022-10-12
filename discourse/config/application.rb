6d32c9eecf9ba0e865bea7750dec1ac5f82b4b48
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-12 14:56:53 -0400

    prep for initial push to Heroku

--- discourse/config/application.rb
+++ discourse/config/application.rb
@@ -41,5 +41,7 @@ module Discourse
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.railties_order = [:all, :main_app] # For Heroku deployment
   end
 end
--- discourse/frontend/src/components/RegisterForm/index.js
+++ discourse/frontend/src/components/RegisterForm/index.js
@@ -11,7 +11,38 @@ function RegisterFormPage() {
     const [email, setEmail] =  useState('');;
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const [errors, setErrors] = useState('');
+    const [parsedErrors,  setParsedErrors] = useState({email: '', username: '', password: ''});
+
+
+    const parseEmail = () => {
+        if ( errors.length && errors.message.includes('Email')) {
+            if (!/\S+@\S+\.\S+/.test(email)) {
+                return  '- Must be a well-formed email address';
+            } else if (email.lengh === 0) {
+                return  'Email must not be blank'
+            } else if (errors.message.includes('taken')) {
+                return "- Email has already been taken"
+            }
+            return ""
+        }
+    }
+
+    const parseUsername = () => {
+        if (username.length < 3) {
+            return 'Username must be at least 3 character'
+        }
+        return ""
+    }
+
+    const parsePassword = () => {
+        if (password.length < 8) {
+            return 'Password must be at least 8 characters';
+        }
+        return ""
+    }
+
+    
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -41,11 +72,12 @@ function RegisterFormPage() {
             {sessionUser ? <Redirect to="/" /> : ''}
             <ul>
                 {errors.message}
+                {parsedErrors.asld}
             </ul>
             <h1>Create an account</h1>
             <form onSubmit={handleSubmit}>
                 <div className='inputGroup'>
-                    <label className={(errors.length > 0 && !/\S+@\S+\.\S+/.test(email)) ? 'errorFieldName' : ''}>Email <span className='errorMessage'>{(errors.length > 0 && !/\S+@\S+\.\S+/.test(email)) ? '- Not a well formed email address' : ''}</span></label>
+                    <label >Email <span className='errorMessage'>{}</span></label>
                     <input type='text' value={email} onChange={(e) => setEmail(e.target.value)} required></input>
                 </div>
 
