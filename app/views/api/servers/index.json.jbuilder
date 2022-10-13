d5a2d98112309851e23ea59361e0479521cc0601
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-13 17:42:35 -0400

    server index skeleton

--- app/views/api/servers/index.json.jbuilder
+++ app/views/api/servers/index.json.jbuilder
@@ -1,5 +1,5 @@
 json.array! (@servers) do |server|
-    json.name server.server_name
+    json.server_name server.server_name
     json.owner server.owner
     json.members server.members
 end
--- frontend/src/components/serverIndexItem/index.js
+++ frontend/src/components/serverIndexItem/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 
-function ServerIndexItem(server) {
-    const dispatch = useDispatch();
+function ServerIndexItem( { server }) {
+    // const dispatch = useDispatch();
 
     return (
-        <li>
-            <h2>ServerIndexItem placeholder</h2>
-            {server.name} - owned by: {server.owner}
+        <li key={server.serverName}>
+            {server.serverName} - owned by: {server.owner.username}
         </li>
     )
 
