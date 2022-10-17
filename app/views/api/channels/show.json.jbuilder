411aa6eef1aaae6e8dd30d4f544b407f0516ac78
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-17 11:57:48 -0400

    useParams working for channel url

---
+++ app/views/api/channels/show.json.jbuilder
@@ -0,0 +1,3 @@
+json.channel do
+    json.extract! @channel, :id, :channel_name, :server
+end
--- frontend/src/components/ServerShow/index.js
+++ frontend/src/components/ServerShow/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import './ServerShow.css'
 
 
 
 
 function ServerShow() {
+    const { id } = useParams();
 
     return (
         <div id='serverShowContainer'>
