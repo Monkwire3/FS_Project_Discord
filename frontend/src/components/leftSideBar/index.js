74836074af729e900f68132f0d43c986eaa928cc
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-19 10:43:16 -0400

    good enough plus svg

--- frontend/src/components/leftSideBar/index.js
+++ frontend/src/components/leftSideBar/index.js
@@ -75,6 +75,15 @@ function LeftSidebar() {
                     
            
                 </div>
+                <div className='channelHeaderTextBox'>
+                    <div>Text channels</div>
+                    <div className='plusSvg'>
+                        <svg>
+                            <polygon fill='#96989d' points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8"></polygon>
+                        </svg>
+                    </div>
+                </div>
+
                 {channelListItems}
             </div>
             <div id='userBar'>
