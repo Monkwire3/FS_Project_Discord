8618183aa796aa33f9e247e960b3736087f1f2f8
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-19 11:29:29 -0400

    backend crud setup

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -11,4 +11,42 @@ class Api::ChannelsController < ApplicationController
         render 'api/channels/show'
     end
 
+    def create
+        @channel = Channel.new(channel_params)
+
+        if @channel.save!
+            render 'api/channels/show'
+        else
+            render json: { errors: @channel.errors.full_messages }
+        end
+    end
+
+    def update
+        @channel = Channel.find(params[:id])
+        if @channel
+            if @channel.update(channel_params)
+                render :show
+            end
+        else
+            render json: { errors: @server.errors.full_messages, status: 422} 
+        end
+    end
+
+    def destroy
+        @channel = Channel.find(params[:id])
+        if @channel
+            @channel.delete()
+            render json: { message: 'server successfully deleted'}
+        else
+            render json: { errors: @channel.errors.full_messages }
+
+        end
+    end
+
+    private
+
+    def channel_params
+        params.require(:channel).permit(:channel_name, :server_id)
+    end
+
 end
