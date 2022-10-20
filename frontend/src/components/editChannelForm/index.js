dabfcc6cbc18134897ed8b7e6fc9a439cdc6f6d0
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-10-20 16:39:39 -0400

    minor aesthetic changes

--- frontend/src/components/editChannelForm/index.js
+++ frontend/src/components/editChannelForm/index.js
@@ -57,6 +57,20 @@ function EditChannelForm({onClose, channel}) {
                 <button>Save Changes</button>
             </div>
         </form>
+        <div onClick={onClose} id="closeButton">
+            <div id="closeButtonCircle">
+            <svg id="closeButtonSvg">
+                <path fill='#b9bbbe' d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z">
+
+                </path>
+
+            </svg>
+            </div>
+            <div>
+                ESC
+            </div>
+
+        </div>
         </div>
         </div>
 
