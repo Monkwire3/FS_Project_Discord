77007bd854ced2dd96f777b8a524231bf57da378
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-11-09 09:39:39 -0500

    refactoring server dropdown modal (no bugs)

--- frontend/src/components/leftSideBar/index.js
+++ frontend/src/components/leftSideBar/index.js
@@ -71,25 +71,9 @@ function LeftSidebar({serverId}) {
     return (
         <div id="leftSidebar">
             <div id='channels'>
-                <div onClick={toggleDropDown} id='channelsHeader'>
-                    <div>{server ? server.serverName : ''}</div>
-                    <div id="dropDownToggle">
-                        <svg className='dropIcon' width={18} height={18}>
-                            <g fill='none' fillRule="evenodd">
-                            <path d="M0 0h18v18H0"></path>
-                            <path stroke="#d8d9da" d="M4.5 4.5l9 9" strokeLinecap="round"></path>
-                            <path stroke="#d8d9da" d="M13.5 4.5l-9 9" strokeLinecap="round"></path>
-                            </g>
-                        </svg>
-                    </div>
-                    <ServerDropDownModal server={server}/>
-                    {/* {dropDownDisplay ? <ServerDropDown /> : ''} */}
-
-
-
-                    
-           
-                </div>
+                {/* {server ? <ServerDropDownModal server={server}/> : '' } */}
+                <ServerDropDownModal serverId={serverId} />
+                
                 <div className='channelHeaderTextBox'>
                     <div>Text channels</div>
                     <div className='plusSvg'>
--- frontend/src/components/serverDropDownModal/ServerDropDownModal.js
+++ frontend/src/components/serverDropDownModal/ServerDropDownModal.js
@@ -1,10 +1,11 @@
 import { Modal } from '../../context/Modal';
 import React, { useState } from 'react';
 import ServerDropDown from '../ServerDropDown';
+import '../leftSideBar/LeftSidebar.css';
 
 
 
-function ServerDropDownModal({server}) {
+function ServerDropDownModal({ serverId }) {
     const [showModal, setShowModal] = useState(false);
 
     const handleClose = () => {
@@ -13,13 +14,27 @@ function ServerDropDownModal({server}) {
     }
 
     return (
-        <>
-        {showModal && (
-            <Modal onClose={handleClose}>
-                <ServerDropDown server={server} onClose={handleClose}/>
-            </Modal>
-        )}
-        </>
+        // <h1>dropdown placeholder </h1>
+        // <div onClick={setShowModal(true)} id='channelsHeader'>
+        <div> // TEMP
+            <div>{serverId}</div>
+            <div id="dropDownToggle">
+                <svg className='dropIcon' width={18} height={18}>
+                    <g fill='none' fillRule="evenodd">
+                        <path d="M0 0h18v18H0"></path>
+                        <path stroke="#d8d9da" d="M4.5 4.5l9 9" strokeLinecap="round"></path>
+                        <path stroke="#d8d9da" d="M13.5 4.5l-9 9" strokeLinecap="round"></path>
+                    </g>
+                </svg>
+            </div>
+            {showModal && (
+                <Modal onClose={handleClose}>
+                    <ServerDropDown server={serverId} onClose={handleClose} />
+                </Modal>
+            )}
+
+        </div>
+
     )
 }
 
