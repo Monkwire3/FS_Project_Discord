5ba7ee9a4cd6867d76f5febd459685013f6b0d42
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-11-10 17:41:58 -0500

    early frontend support for friends

--- frontend/src/components/DirectMessagesFull/index.js
+++ frontend/src/components/DirectMessagesFull/index.js
@@ -1,7 +1,14 @@
+import MeLeftBar from "../MeLeftBar";
+import ServerIndex from "../ServerIndex";
 
 
 function DirectMessagesFull() {
-
+    return (
+        <>
+        <ServerIndex />
+        <MeLeftBar />
+        </>
+    )
 }
 
 export default DirectMessagesFull;
--- frontend/src/components/newServerFormModal/index.js
+++ frontend/src/components/newServerFormModal/index.js
@@ -10,7 +10,7 @@ function NewServerFormModal() {
 
     return (
         <>
-        <div className="addServerListItem" onClick={() => setShowModal(true)}>
+        <div className='serverListItem' onClick={() => setShowModal(true)}>
             <svg viewBox='0 0 24 24'>
                 <path fill="#3aa55d" d="M20 11.1111H12.8889V4H11.1111V11.1111H4V12.8889H11.1111V20H12.8889V12.8889H20V11.1111Z"></path>
             </svg>
