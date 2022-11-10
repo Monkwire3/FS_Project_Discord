b99dccbbf112e3bd88db92ac3ab75143f7be3eb0
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-11-10 11:49:31 -0500

    change add server button to use svg

--- frontend/src/components/newServerFormModal/index.js
+++ frontend/src/components/newServerFormModal/index.js
@@ -10,7 +10,12 @@ function NewServerFormModal() {
 
     return (
         <>
-        <div className="addServerListItem" onClick={() => setShowModal(true)}>+</div>
+        <div className="addServerListItem" onClick={() => setShowModal(true)}>
+            <svg viewBox='0 0 24 24'>
+                <path fill="#3aa55d" d="M20 11.1111H12.8889V4H11.1111V11.1111H4V12.8889H11.1111V20H12.8889V12.8889H20V11.1111Z"></path>
+            </svg>
+
+        </div>
         {showModal && (
             <Modal onClose={(e) => setShowModal(false)}>
                 <CreateServerForm  />
@@ -19,5 +24,6 @@ function NewServerFormModal() {
         </>
     )
 }
+
 
 export default NewServerFormModal;
