46573a53546ee605724d47cb50004ca969d76449
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-12-08 12:35:41 -0500

    fixed chat redirect

--- frontend/src/components/FriendsList/FriendsListIndexItem.js
+++ frontend/src/components/FriendsList/FriendsListIndexItem.js
@@ -8,24 +8,23 @@ import { useHistory } from 'react-router-dom';
 function FriendsListIndexItem({friend}) {
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
-    const [chatId, setChatId] = useState('');
 
-    useEffect(() => {
-        debugger
-    }, [])
+    const history = useHistory();
 
 
 
-    const receieveFunc = (openDirectMessage) => {
-        const directMessage = openDirectMessage();
-        console.log('dirmessage: ', directMessage);
+ 
+    const openDirectMessage = async () => {
+        const res = await dispatch(createChat({title: `${friend.username}, ${sessionUser.username}`, user_1: sessionUser.id, user_2: friend.id}))
 
+
+        history.push(`@me/${res.id}`)
+
+        console.log('resid ', res.id)
+
+        return res
     }
-    const openDirectMessage = () => {
-        return dispatch(createChat({title: `${friend.username}, ${sessionUser.username}`, user_1: sessionUser.id, user_2: friend.id}))
-    }
 
-    debugger
 
 
   
@@ -33,13 +32,11 @@ function FriendsListIndexItem({friend}) {
         <div className="friends-list-index-item">
             <div>{friend.username}</div>
             <div className="icon-group">
-                <a href={`/${openDirectMessage.id}`}>
-                <div className='icon' >
+                <div className='icon' onClick={openDirectMessage}>
                     <svg viewBox="0 0 24 24">
                         <path fill='#b9bbbe' d='M4.79805 3C3.80445 3 2.99805 3.8055 2.99805 4.8V15.6C2.99805 16.5936 3.80445 17.4 4.79805 17.4H7.49805V21L11.098 17.4H19.198C20.1925 17.4 20.998 16.5936 20.998 15.6V4.8C20.998 3.8055 20.1925 3 19.198 3H4.79805Z'></path>
                     </svg>
                 </div>
-                </a>
                 <div className='icon'>
                     <svg viewBox="0 0 24 24">
                         <path fill="#b9bbbe" d='M12 16c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2 .8954305-2 2-2zm0-6c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2 .8954305-2 2-2zm0-6c1.1045695 0 2 .8954305 2 2s-.8954305 2-2 2-2-.8954305-2-2 .8954305-2 2-2z'></path>
