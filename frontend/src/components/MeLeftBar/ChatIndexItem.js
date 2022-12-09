ae43b13273b2e50784f615f632e7990c5438d01f
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-12-09 12:14:49 -0500

    fix direct messages links

--- frontend/src/components/MeLeftBar/ChatIndexItem.js
+++ frontend/src/components/MeLeftBar/ChatIndexItem.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function ChatIndexItem({chat}) {
 
     return (
-        <Link to={`/@me/${chat.id}`}><div className="chat-index-item">{chat.title}</div></Link>
+        <a href={`/channels/@me/${chat.id}`}><div className="chat-index-item">{chat.title}</div></a>
     )
 
 
