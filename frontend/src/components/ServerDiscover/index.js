40159158fc950cbe5c16e5f0a2ef94a0dfba3550
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-12-11 21:20:18 -0500

    create server discover component

---
+++ frontend/src/components/ServerDiscover/index.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+
+function ServerDiscover() {
+    const [unjoinedServers, setUnjoinedServers]  = useState([]);
+
+    useEffect(() => {
+
+    }, [])
+
+}
+
+export default ServerDiscover;
