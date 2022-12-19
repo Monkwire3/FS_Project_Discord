1b8d0bf38224b6466eee992c0c85896500d25124
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-12-19 12:48:42 -0500

    fixed chat redirect issue

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -30,7 +30,6 @@ class Api::UsersController < ApplicationController
   end
 
   def accept_friend_request
-    debugger
     @request = Friend.where(requestee_id: params[:requestee_id], requester_id: params[:requester_id])
     @request.update(accepted: true)
     @requests = Friend.where(requester_id: current_user.id, accepted: false).or(Friend.where(requestee_id: current_user.id, accepted: false)) 
