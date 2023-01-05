b67c1721cdf4ff8278a15ee809aa44831118f42a
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2023-01-05 13:01:17 -0500

    fill out messages.js

--- frontend/src/store/messages.js
+++ frontend/src/store/messages.js
@@ -1,13 +1,13 @@
 import csrfFetch from "./csrfFetch";
 
-const RECIEVE_MESSAGES = 'messages/receiveMessages'
+const RECEIVE_MESSAGES = 'messages/receiveMessages'
 const ADD_MESSAGE = 'messages/addMessage'
 const REMOVE_MESSAGE = 'messages/removeMessage'
 const UPDATE_MESSAGE = 'messages/updateMessage'
 
 const getMessages = (messages) => {
     return {
-        type: RECIEVE_MESSAGES,
+        type: RECEIVE_MESSAGES,
         payload: messages
     }
 }
@@ -52,5 +52,57 @@ export const fetchMessages = ({channelId, chatId}) => async(dispatch) => {
 }
 
 export const createMessage = (message) => async(dispatch) => {
-    const res = await csrfFetch(`/`)
+    // there may be a parsing issue with channelId (as opposed to channel_id)
+    const res = await csrfFetch(`/api/messages`, {
+        method: 'POST',
+        body: JSON.stringify({
+            message: {
+                body: message.body,
+                senderId: message.senderId,
+                chatId: message.chatId,
+                channelId: message.channelId
+            }
+        })
+    })
+
+    const data = await res.json();
+    dispatch(addMessage(data))
+}
+
+export const deleteMessage = (message) => async(dispatch) => {
+    const res = await csrfFetch(`/api/messages/${message.id}`, {method: 'DELETE'});
+
+    dispatch(removeMessage(message.id));
+    return res;
+}
+
+export const editMessage = (message) => async(dispatch) => {
+    const res = await csrfFetch(`/api/messages/${message.id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({
+            body: message.body
+        })
+    })
+
+    const data = await res.json();
+    dispatch(updateMessage(data))
+}
+
+const messagesReducer = (state = {}, action) => {
+    const nextState = {...state};
+
+    switch (action.type) {
+        case RECIEVE_MESSAGES:
+            return action.payload;
+        case ADD_MESSAGE:
+            return {...state, ...action.payload};
+        case REMOVE_MESSAGE:
+            delete nextState.action.payload.id;
+            return nextState;
+        case UPDATE_MESSAGE:
+            nextState[action.payload.id] = action.payload;
+            return nextState;
+        default:
+            return state
+    }
 }
